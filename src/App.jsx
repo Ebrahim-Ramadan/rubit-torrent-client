9a630cdbd2ff40d10871c24390be2d0bee301225
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-29 16:51:11 +0200

    fixed some desktop looks

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ export default function App() {
         />
       </div>
 </section>
-      <main className="flex-1">
+      <main className="flex-1 md:mt-[-20rem]">
         <section className="w-full">
           <div className=" px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
@@ -95,7 +95,7 @@ export default function App() {
                 </a> */}
                 <a href='https://github.com/spectre-xenon/rubit' className='flex flex-row gap-2 items-center text-neutral-400'>
                 Documentation
-                <ArrowRightIcon size='20' color='#454545' />
+                <ArrowRightIcon size='20' color='#CCCCCC' />
               </a>
               </div>
             </div>
